e2ca9aa40c75038c8d3d12655cbbb2379db05370
Zahra Bahmanpour <z.bmnpr@gmail.com>
2024-06-06 05:29:22 +0330

    customizing screens and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
+      // screens: {
+      //   'sm': '640px',
+      //   'md': '768px',
+      //   'lg': '1024px',
+      //   'xl': '1280px',
+      //   '2xl': '1536px',
+      // },
+      // spacing: {
+      //   1: "8px",
+      //   2: "12px",
+      //   3: "16px",
+      //   4: "24px",
+      //   5: "32px",
+      //   6: "48px",
+      // },
       colors: {
         gray: "	#9e9aa7",
         lightGray: "	#F3F4F6",
